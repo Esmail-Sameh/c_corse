013b4f815900fda9b5619987d30817a3bacb78d6
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-03-14 22:34:05 +0200

    first

---
+++ 1/print.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello Esmail\n");
+    printf("Hello Aboood\n");
+
+    printf("%d\n", 150);
+    printf("%lf", 150.12);
+
+    return 0;
+}
---
+++ 1/var dec.c
@@ -0,0 +1,20 @@
+// Variables Declaration
+
+#include <stdio.h>
+
+int main(){
+
+    int num1 = 10;
+    int num2 = 5;
+    num1 += num2;
+    char c = 'r';
+    char cc[] = "Esmail Abdo Ahmed";
+
+
+    printf("%i\n", num1);
+    printf("%c\n", c);
+    printf("%s\n", cc);
+
+
+
+}
